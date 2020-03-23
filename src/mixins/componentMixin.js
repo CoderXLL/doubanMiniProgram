a2f2a2396950cbccdd935911975dc9401244b4dc
肖乐 <lele.xiao@melot.cn>
2020-03-23 13:57:59 +0800

    fix something

--- src/mixins/componentMixin.js
+++ src/mixins/componentMixin.js
@@ -2,14 +2,14 @@ export default {
   //component lifecircle
   created() {
     //创建组件
-    console.log("component----- created 触发")
+    console.log(this.$is + "-----component----- created 触发")
   },
   attached() {
     //使用组件
-    console.log("component----- attached 触发")
+    console.log(this.$is + "-----component----- attached 触发")
   },
   detached() {
     //销毁组件
-    console.log("component----- detached 触发")
+    console.log(this.$is + "-----component----- detached 触发")
   }
 }
--- src/mixins/pageMixin.js
+++ src/mixins/pageMixin.js
@@ -2,22 +2,22 @@ export default {
   //page lifecircle
   onLoad() {
     //监听页面加载
-    console.log("page----- onLoad 触发");
+    console.log(this.$is + "-----page----- onLoad 触发");
   },
   onReady() {
     //监听页面初次渲染完成
-    console.log("page----- onReady 触发");
+    console.log(this.$is + "page----- onReady 触发");
   },
   onShow() {
     //监听页面显示
-    console.log("page----- onShow 触发");
+    console.log(this.$is + "page----- onShow 触发");
   },
   onHide() {
     //监听页面隐藏
-    console.log("page----- onHide 触发");
+    console.log(this.$is + "page----- onHide 触发");
   },
   onUnload() {
     //监听页面卸载
-    console.log("page----- onUnload 触发");
+    console.log(this.$is + "page----- onUnload 触发");
   }
 }
